eb6066bf4269e78989983b1dcf404573e38023d8
fourcows <fourcows@163.com>
2021-12-11 10:59:29 +0800

    初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    lang: 'zh-CN',
+    title: 'nben-cloud',
+    description: '使用nben-cloud开发web项目',
+
+    themeConfig: {
+        navbar: [
+            // NavbarItem
+            {
+                text: '后端文档',
+                link: '/backend/',
+            },
+            {
+                text: '前端文档',
+                link: '/frontend/',
+            },
+            {
+                text: 'Gitee',
+                link: 'https://gitee.com/fourcows/nben-cloud',
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/fourcows/nben-cloud',
+            },
+        ],
+    }
+}
